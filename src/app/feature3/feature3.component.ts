6ffc0c0b640a3d908aa56858bd2d257b8b70fbde
sneha-kolli <kollisnehagowri@gmail.com>
2025-07-22 10:53:00 +0530

    feature 3 added

---
+++ src/app/feature3/feature3.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature3',
+  imports: [],
+  templateUrl: './feature3.component.html',
+  styleUrl: './feature3.component.scss'
+})
+export class Feature3Component {
+
+}
